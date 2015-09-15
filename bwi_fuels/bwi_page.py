6f6073c58648407c495931678adb1d584e9105df
ThorsteinnAdal <thorsteinn.adalsteinsson@arktech.net>
2015-09-15 21:50:50 +0000

    simplified bwi_page

--- bwi_fuels/bwi_page.py
+++ bwi_fuels/bwi_page.py
@@ -6,7 +6,7 @@ import re
 import json
 from bs4 import BeautifulSoup
 
-def map_bwi_page(country, port):
+def bwi_page(country, port):
     opener = urllib2.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     url = 'http://www.bunkerworld.com/prices/port/{c}/{p}/'.format(c = country, p = port)
@@ -15,21 +15,20 @@ def map_bwi_page(country, port):
 
     soup = BeautifulSoup(page.read(), 'html.parser')
 
-    fuelHeaders = soup.find_all('div', {'class':"content_block_title"})
+    fuelHeaders = soup.find_all('div', {'class':"content_block_title"})  # here are all the headers that matter
 
-    db = {'table0':{'fuel:':'IFO380','iso':'RMG380 RMH380', 'report':'daily','table':[]},
-          'table1':{'fuel:':'IFO180','iso':'RME180 RMF180', 'report':'daily','table':[]},
-          'table2':{'fuel:':'MGO','iso':'DMA DMX', 'report':'daily','table':[]},
-          'table3':{'fuel:':'LSMGO','iso': "0.1% sulfur", 'report':'daily','table':[]},
-          'table4':{'fuel:':'IFO380','iso':'RMG380 RMH380', 'report':'monthly','table':[]},
-          'table5':{'fuel:':'IFO180','iso':'RME180 RMF180', 'report':'monthly','table':[]},
-          'table6':{'fuel:':'MGO','iso':'DMA DMX', 'report':'monthly','table':[]},
-          'table7':{'fuel:':'LSMGO','iso': "0.1% sulfur", 'report':'monthly','table':[]},
-          'table8':{'fuel:':'IFO380','iso':'RMG380 RMH380', 'report':'30 day','table':[]},
-          'table9':{'fuel:':'IFO180','iso':'RME180 RMF180', 'report':'30 day','table':[]},
-          'table10':{'fuel:':'MGO','iso':'DMA DMX', 'report':'30 day','table':[]},
-          'table11':{'fuel:':'LSMGO','iso': "0.1% sulfur", 'report':'30 day','table':[]}}
-
+    db = {'T0':{'fuel:':'IFO380','iso':'RMG380 RMH380', 'report':'daily','table':[]},
+          'T1':{'fuel:':'IFO180','iso':'RME180 RMF180', 'report':'daily','table':[]},
+          'T2':{'fuel:':'MGO','iso':'DMA DMX', 'report':'daily','table':[]},
+          'T3':{'fuel:':'LSMGO','iso': "0.1% sulfur", 'report':'daily','table':[]},
+          'T4':{'fuel:':'IFO380','iso':'RMG380 RMH380', 'report':'monthly','table':[]},
+          'T5':{'fuel:':'IFO180','iso':'RME180 RMF180', 'report':'monthly','table':[]},
+          'T6':{'fuel:':'MGO','iso':'DMA DMX', 'report':'monthly','table':[]},
+          'T7':{'fuel:':'LSMGO','iso': "0.1% sulfur", 'report':'monthly','table':[]},
+          'T8':{'fuel:':'IFO380','iso':'RMG380 RMH380', 'report':'30 day','table':[]},
+          'T9':{'fuel:':'IFO180','iso':'RME180 RMF180', 'report':'30 day','table':[]},
+          'T10':{'fuel:':'MGO','iso':'DMA DMX', 'report':'30 day','table':[]},
+          'T11':{'fuel:':'LSMGO','iso': "0.1% sulfur", 'report':'30 day','table':[]}}
 
     tables = soup.find_all('table', {'class':"item_table row_borders"})
 
@@ -42,12 +41,13 @@ def map_bwi_page(country, port):
             cols = row.find_all('td')
             cols = [ele.text.strip() for ele in cols]
             this_table.append([ele for ele in cols if ele]) # Get rid of empty values
+
+        db['port'] = "{}{}".format(country.upper(),port.upper())
         db['table%s'% index]['table'] = this_table
-        data[approved_headers[index]] = this_table
         this_table = []
         index +=1
 
     return db
 
 
-print map_bwi_page('nl', 'rtm') #others Singapore sg/sin/, Houston us/hou/, Fujairah ae/fjr/
+# print map_bwi_page('nl', 'rtm') #others Singapore sg/sin/, Houston us/hou/, Fujairah ae/fjr/
